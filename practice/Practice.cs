1fd317eec401bd37e60ff9f83173d8edd6cd7146
rmghost <youngenli@gmail.com>
2018-07-17 09:43:27 -0500

    initial files

---
+++ practice/Practice.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace practice
+{
+    public class Practice
+    {
+        public Practice()
+        {
+            List<string> words = new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics"};
+        }
+    }
+}
