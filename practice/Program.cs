09b78f2c44f9412dff4e2df84c33bd6f64fa09e3
rmghost <youngenli@gmail.com>
2018-07-17 10:04:03 -0500

    finished question one

--- practice/Program.cs
+++ practice/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace practice
 {
@@ -7,6 +9,12 @@ namespace practice
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            List<string> words = new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics" };
+
+            var wordSearch = words.Where(w => w.Contains("th")).Count();
+
+            Console.WriteLine(wordSearch);
+            Console.ReadKey();
         }
     }
 }
