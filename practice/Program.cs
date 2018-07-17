e0099ac007264f60a1e21229335b4ee0d1684b90
rmghost <youngenli@gmail.com>
2018-07-17 10:51:05 -0500

    finished question 2

--- practice/Program.cs
+++ practice/Program.cs
@@ -8,13 +8,25 @@ namespace practice
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<string> names = new List<string>() { "Mike", "Dan", "Scott", "Nick", "Mike" };
+
+
+            List<string> distinct = names.Distinct().ToList();
+            foreach (string value in distinct)
+            {
+                Console.WriteLine("{0}", value);
+            }
+
+        }
+
+        public void Contains(){
             List<string> words = new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics" };
-
             var wordSearch = words.Where(w => w.Contains("th")).Count();
-
             Console.WriteLine(wordSearch);
-            Console.ReadKey();
+        }
+
+        public void Duplicates(){
+            
         }
     }
 }
