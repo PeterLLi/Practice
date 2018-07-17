5ae6585521f7272d859f8b34f680a52596c04a97
rmghost <youngenli@gmail.com>
2018-07-17 16:20:30 -0500

    Added sort and drop lowest number functionality

--- practice/Program.cs
+++ practice/Program.cs
@@ -8,25 +8,57 @@ namespace practice
     {
         public static void Main(string[] args)
         {
-            List<string> names = new List<string>() { "Mike", "Dan", "Scott", "Nick", "Mike" };
+            //Problem one
+            List<string> words = new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics" };
+            var wordSearch = words.Where(w => w.Contains("th")).Count();
+            Console.WriteLine(wordSearch);
 
+            //Problem two
+            List<string> names = new List<string>() { "Mike", "Dan", "Scott", "Nick", "Mike" };
+            foreach (string value in names)
+            {
+                Console.WriteLine("Before: {0}", value);
+            }
 
             List<string> distinct = names.Distinct().ToList();
             foreach (string value in distinct)
             {
-                Console.WriteLine("{0}", value);
+                Console.WriteLine("After: {0}", value);
             }
 
-        }
+            //Problem three
+            List<string> classGrades = new List<string>(){
+                "80,100,92,89,65",
+                "93,81,78,84,69",
+                "73,88,83,99,64",
+                "98,100,66,74,55"
+            };
+            var usersGrades = classGrades[0].Split(',').Select(s => int.Parse(s)).ToList();
+            var ascendingOrder = usersGrades.OrderBy(i => i).ToList();
+            Console.WriteLine("Sort");
+            foreach(var item in ascendingOrder){
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("");
 
-        public void Contains(){
-            List<string> words = new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics" };
-            var wordSearch = words.Where(w => w.Contains("th")).Count();
-            Console.WriteLine(wordSearch);
-        }
+            var dropLowest = from lowestScore in ascendingOrder
+                                            where lowestScore > ascendingOrder.Min()
+                                            select lowestScore;
+            Console.WriteLine("Drop lowest");
+            foreach(var item in dropLowest){
+                Console.WriteLine(item);
+            }
 
-        public void Duplicates(){
-            
+            //Problem 4
+            string str = "terrill";
+            List<string> charCounts = str.GroupBy(ch => ch).OrderBy(item => item.Key).Select(item => item.Key + "=" + item.Count().ToString()).ToList();
+
+            foreach (string s in charCounts)
+            {
+                Console.WriteLine(s);
+
+            }
+            Console.ReadKey();
         }
     }
 }
